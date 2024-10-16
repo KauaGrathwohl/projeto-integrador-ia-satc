1b729f03dae8d752e9d7a2d6aa17aa9312c1ed15
kauagrathwohl <kauagrathwohl095@gmail.com>
2024-10-16 13:01:38 -0300

    Adicionando mensagens de exception nos endpoints de pacientes e receitas

--- src/main/java/com/nutrisys/api/paciente/PacienteController.java
+++ src/main/java/com/nutrisys/api/paciente/PacienteController.java
@@ -1,5 +1,7 @@
 package com.nutrisys.api.paciente;
 
+import com.nutrisys.api.exception.BadRequestException;
+import com.nutrisys.api.exception.ResourceNotFoundException;
 import com.nutrisys.api.paciente.dto.CreatePacienteDto;
 import com.nutrisys.api.paciente.dto.CreatedPacienteDto;
 import com.nutrisys.api.paciente.dto.DetailPacienteDto;
@@ -18,16 +20,28 @@ public class PacienteController {
 
     @PostMapping
     public CreatedPacienteDto createPaciente(@RequestBody CreatePacienteDto createPacienteDto) {
-        return pacienteService.createPaciente(createPacienteDto);
+        try {
+            return pacienteService.createPaciente(createPacienteDto);
+        } catch (Exception e) {
+            throw new BadRequestException("Error ao criar paciente: " + e.getMessage());
+        }
     }
 
     @GetMapping
     public List<ListPacienteDto> listPacientes() {
-        return pacienteService.listPacienteDtos();
+        try {
+            return pacienteService.listPacienteDtos();
+        } catch (Exception e) {
+            throw new ResourceNotFoundException("Erro ao listar pacientes: " + e.getMessage());
+        }
     }
 
     @GetMapping("/detalhes/{idPaciente}")
     public DetailPacienteDto getDetailPaciente(@PathVariable("idPaciente") Long idPaciente) {
-        return pacienteService.getDetailPaciente(idPaciente);
+        try {
+            return pacienteService.getDetailPaciente(idPaciente);
+        } catch (Exception e) {
+            throw new ResourceNotFoundException("Erro ao mostrar detalhes do paciente: " + e.getMessage());
+        }
     }
 }
